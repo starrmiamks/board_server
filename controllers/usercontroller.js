b5dd252fd4a64712b76fe643ea10927e2152ca1c
Mia Starr <starrmiamks@gmail.com>
2021-05-06 23:50:36 -0400

    updated usercontroller

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require('../db').import('../models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const validateSession = require('../middleware/validate-session');
 
 router.post('/register', function (req, res) {
     User.create({
@@ -9,6 +10,7 @@ router.post('/register', function (req, res) {
         password: bcrypt.hashSync(req.body.user.password, 13),
         firstName: req.body.user.firstName,
         lastName: req.body.user.lastName,
+        admin: req.body.user.admin
     })
         .then(
             function registerSuccess(user) {
@@ -50,7 +52,79 @@ router.post('/login', function (req, res) {
         .catch(err => res.status(500).json({ error: err }))
 });
 
+router.put('/admin/:id', validateSession, (req, res) => {
+    if (!req.err && req.body.user.admin) {
+        const updateUser = {
+            firstName: req.body.user.firstName,
+            lastName: req.body.user.lastName,
+            email: req.body.user.email,
+            admin: req.body.user.admin,
+        };
+        if (req.body.user.password != '') {
+            updateUser.password = bcrypt.hashSync(req.body.user.password, 13)
+            console.log(req.body.user.password)
+        }
+        const query = { where: { id: req.params.id } };
+        User.update(updateUser, query)
+            .then((user) => res.status(201).json({ mesage: `${user} records updated` }))
+            .catch((err) => res.status(500).json({ error: err }));
+    }
+})
 
+router.put('/', validateSession, (req, res) => {
+    let userId = req.user.id;
+
+    const updateUser = {
+        firstName: req.body.user.firstName,
+        lastName: req.body.userlastName,
+        email: req.body.user.email,
+    };
+    if (req.body.user.password != '') {
+        updateUser.password = bcrypt.hashSync(req.body.user.password, 13)
+        console.log(req.body.password)
+    }
+    const query = { where: { id: userId } };
+    User.update(updateUser, query)
+        .then((user) => res.status(200).json({ message: `${user} records updated` }))
+        .catch((err) => res.status(500).json({ error: err }));
+});
+
+router.delete('/:id', validateSession, function (req, res) {
+    if (!req.err & req.body.user.admin) {
+        const query = { where: { id: req.params.id } };
+
+        User.destroy(query)
+            .then(() => res.status(200).json({ message: 'User Deleted' }))
+            .catch((err) => res.status(500).json({ error: err }));
+    } else {
+        return res.status(500).send({ message: 'Not Authorized' });
+    }
+})
+
+router.get('/one/:id', validateSession, (req, res) => {
+    User.findOne({
+        where: { id: req.params.id }
+    })
+        .then((location) => res.status(200).json(location))
+        .catch((err) => res.status(500).json({ error: err }));
+});
+
+router.get('/', validateSession, (req, res) => {
+    
+    user.findOne({ where: { id: req.user.id }})
+    .then((user) => res.status(200).json(user))
+    .catch((err) => res.status(500).json({ error: err}));
+});
+
+router.get('/all', validateSession, (req, res) => {
+    if (!req.err && req.body.user.admin){
+        User.findALL()
+        .then((user) => res.status(200).json(user))
+        .catch((err) => res.status(500).json({ error: err }));
+    }else{
+        return res.status(500).send({ message: 'Not Authorized' });
+    }
+})
 
 module.exports = router;
 
